7ea5576db6905f99f76ab3bf81ec65309837f594
dlserodio <lanzillottaprojetos@gmail.com>
2024-01-04 12:02:10 -0300

    Reorganized in OOP

--- JointScanner.py
+++ JointScanner.py
@@ -3,28 +3,45 @@ import mediapipe as mp
 
 mp_draw = mp.solutions.drawing_utils
 mp_pose = mp.solutions.pose
-pose = mp_pose.Pose()
 
-image_feed = cv2.VideoCapture(0)
+class BodyScanner:
+    def __init__(self, mode, onlyUp, smooth, detectCon, trackCon):
+        self.pose = mp_pose.Pose(mode, onlyUp, smooth, detectCon, trackCon)
 
-while True:
-    success, frame = image_feed.read()
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    
-    results = pose.process(frame_rgb)
-    print(results.pose_landmarks)
-    
-    if cv2.waitKey(1) == 27:
-        break
-    elif results.pose_landmarks:
-        mp_draw.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
-        for id, landmarks in enumerate(results.pose_landmarks.landmark):
-            h, w, c = frame.shape
-            print(id, landmarks)
-            cx, cy = int(landmarks.x*w), int(landmarks.y*h)
-            cv2.circle(frame, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+    def process_image(self, image):
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        results = self.pose.process(image_rgb)
 
+        if results.pose_landmarks:
+            mp_draw.draw_landmarks(image, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+            for id, landmarks in enumerate(results.pose_landmarks.landmark):
+                h, w, c = image.shape
+                cx, cy = int(landmarks.x * w), int(landmarks.y * h)
+                cv2.circle(image, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
 
-    cv2.imshow('Live Video', frame)
-cv2.destroyAllWindows()
-image_feed.release()
+        return image
+
+def main():
+    image_feed = cv2.VideoCapture(0)
+    mode = False
+    onlyUp = True
+    smooth = True
+    detectCon = True
+    trackCon = True
+
+    body_scanner = BodyScanner(mode, onlyUp, smooth, detectCon, trackCon)
+
+    while True:
+        success, frame = image_feed.read()
+        processed_frame = body_scanner.process_image(frame)
+
+        if cv2.waitKey(1) == 27:
+            break
+
+        cv2.imshow('Live Video', processed_frame)
+
+    cv2.destroyAllWindows()
+    image_feed.release()
+
+if __name__ == '__main__':
+    main()
